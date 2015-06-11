21744d868e48af8ea081e6e8f7ae5190bf252b73
indra.as <indra.as@004ACE0315.nu-ace.ad-ins.com>
2015-06-11 14:18:03 +0700

    fix pagging general code

--- FinalProjectPromise/src/general/GeneralCodeManager.java
+++ FinalProjectPromise/src/general/GeneralCodeManager.java
@@ -3,7 +3,9 @@ package general;
 import ibatis.IbatisHelper;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
@@ -15,12 +17,18 @@ public class GeneralCodeManager {
 		this.ibatis = IbatisHelper.getSqlMapInstance();
 	}
 
-	public ArrayList<GeneralCodeBean> getAllGeneralCode() {
-		ArrayList<GeneralCodeBean> arr = null;
+	public List<GeneralCodeBean> getAllGeneralCode(String col, String input,
+			Integer begin, Integer end) {
+		List<GeneralCodeBean> arr = null;
+		Map map = new HashMap();
+		map.put("col", col);
+		map.put("input", input);
+		map.put("begin", begin);
+		map.put("end", end);
 
 		try {
-			arr = (ArrayList<GeneralCodeBean>) this.ibatis.queryForList(
-					"genCode.selectGeneralCode", null);
+			arr = (List<GeneralCodeBean>) this.ibatis.queryForList(
+					"genCode.selectGeneralCode", map);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,8 +62,7 @@ public class GeneralCodeManager {
 
 	}
 
-	public void deleteGeneralCodeByCodeId(String genCodeId)
-			throws SQLException {
+	public void deleteGeneralCodeByCodeId(String genCodeId) throws SQLException {
 
 		try {
 			this.ibatis.startTransaction();
