3d58d591aeb30f0177f27e1996573ee10936b0c6
indra.as <indra.as@004ACE0315.nu-ace.ad-ins.com>
2015-06-11 16:56:07 +0700

    rank

--- FinalProjectPromise/src/employee/RankEmpManager.java
+++ FinalProjectPromise/src/employee/RankEmpManager.java
@@ -4,55 +4,68 @@ import ibatis.IbatisHelper;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class RankEmpManager {
-	
+
 	private SqlMapClient ibatis;
-	
-	public RankEmpManager(){
+
+	public RankEmpManager() {
 		this.ibatis = IbatisHelper.getSqlMapInstance();
 	}
-	
-	public List<EmployeeBean> getAllEmployeeRank() throws ClassNotFoundException, SQLException {
-		List<EmployeeBean> list = new ArrayList<EmployeeBean>();
-		list = this.ibatis.queryForList("employee.getAllEmployeeRank", null);
+
+	public List<RankEmpBean> getAllEmployeeRank(String col, String input,
+			Integer pageNum, Integer pageSize) throws ClassNotFoundException,
+			SQLException {
+
+		int begin = (pageNum - 1) * pageSize;
+		int end = pageNum * pageSize;
+
+		Map map = new HashMap();
+		map.put("col", col);
+		map.put("input", input);
+		map.put("begin", begin);
+		map.put("end", end);
+
+		List<RankEmpBean> list = this.ibatis.queryForList(
+				"rank.getAllEmployeeRank", map);
 		return list;
 	}
-	
-	public void insertEmployeeRank(EmployeeBean eb) throws ClassNotFoundException, SQLException{
-		Integer autoRankId = (Integer) this.ibatis.queryForObject("employee.getCountEmployeeRankId", null);
-		if (autoRankId == null){
-			autoRankId = 1;
-		}
-		eb.setEmployeeId(autoRankId);
+
+	public void insertEmployeeRank(RankEmpBean eb)
+			throws ClassNotFoundException, SQLException {
+
 		try {
 			this.ibatis.startTransaction();
-			this.ibatis.insert("employee.insertEmployeeRank", eb);
+			this.ibatis.insert("rank.insertEmployeeRank", eb);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO: handle exception
 			this.ibatis.endTransaction();
 		}
 	}
-	
-	public void updateEmployeeRank(EmployeeBean eb) throws ClassNotFoundException, SQLException{
+
+	public void updateEmployeeRank(RankEmpBean eb)
+			throws ClassNotFoundException, SQLException {
 		try {
 			this.ibatis.startTransaction();
-			this.ibatis.update("employee.updateEmployeeRank", eb);
+			this.ibatis.update("rank.updateEmployeeRank", eb);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO: handle exception
 			this.ibatis.endTransaction();
 		}
 	}
-	
-	public void deleteEmployeeRank(Integer EmployeeId) throws ClassNotFoundException, SQLException{
+
+	public void deleteEmployeeRank(Integer EmployeeId)
+			throws ClassNotFoundException, SQLException {
 		try {
 			this.ibatis.startTransaction();
-			this.ibatis.delete("employee.deleteEmployeeRank", EmployeeId);
+			this.ibatis.delete("rank.deleteEmployeeRank", EmployeeId);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO: handle exception
