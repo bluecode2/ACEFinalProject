694f7688d9e3de4de7816ca7063e92a15f094205
juanrio <juanrio@012ACE0315.nu-ace.ad-ins.com>
2015-06-11 17:55:52 +0700

    generalparam

--- FinalProjectPromise/src/general/GeneralParamForm.java
+++ FinalProjectPromise/src/general/GeneralParamForm.java
@@ -1,11 +1,14 @@
 package general;
 
 import java.util.ArrayList;
+
 import org.apache.struts.action.ActionForm;
 
 public class GeneralParamForm extends ActionForm {
 
 	private ArrayList<GeneralParamBean> arrList = new ArrayList<GeneralParamBean>();
+	private String searchCategory;
+	private String searchItem;
 
 	public ArrayList<GeneralParamBean> getArrList() {
 		return arrList;
@@ -13,5 +16,21 @@ public class GeneralParamForm extends ActionForm {
 
 	public void setArrList(ArrayList<GeneralParamBean> arrList) {
 		this.arrList = arrList;
+	}
+
+	public String getSearchCategory() {
+		return searchCategory;
+	}
+
+	public void setSearchCategory(String searchCategory) {
+		this.searchCategory = searchCategory;
+	}
+
+	public String getSearchItem() {
+		return searchItem;
+	}
+
+	public void setSearchItem(String searchItem) {
+		this.searchItem = searchItem;
 	}
 }
