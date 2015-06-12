a23e041b711d97c63030d03d012d1bf9559f6004
indra.as <indra.as@004ACE0315.nu-ace.ad-ins.com>
2015-06-12 10:18:11 +0700

    update role

--- FinalProjectPromise/src/user/UserRoleManager.java
+++ FinalProjectPromise/src/user/UserRoleManager.java
@@ -1,5 +1,6 @@
 package user;
 
+import general.GeneralCodeBean;
 import ibatis.IbatisHelper;
 
 import java.sql.SQLException;
@@ -11,56 +12,71 @@ import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class UserRoleManager {
 	private SqlMapClient ibatis;
-	
-	public UserRoleManager(){
+
+	public UserRoleManager() {
 		this.ibatis = IbatisHelper.getSqlMapInstance();
 	}
-	
-	public List<UserRoleBean> getUserRole() throws SQLException{
-		List<UserRoleBean> listResult = this.ibatis.queryForList("userRole.getUserRole", null);
-		return listResult;
+
+	public List<UserRoleBean> getUserRole(String col, String input,
+			Integer pageNum, Integer pageSize) throws SQLException {
+		int begin = (pageNum - 1) * pageSize;
+		int end = pageNum * pageSize;
+
+		Map map = new HashMap();
+		map.put("searchField", col);
+		map.put("searchValue", input);
+		map.put("begin", begin);
+		map.put("end", end);
+
+		List<UserRoleBean> arr = (List<UserRoleBean>) this.ibatis.queryForList(
+				"userRole.getUserRole", map);
+
+		return arr;
 	}
-	
-	public void insertUserRole(UserRoleBean userRoleBean) throws SQLException{
+
+	public void insertUserRole(UserRoleBean userRoleBean) throws SQLException {
 		try {
 			this.ibatis.startTransaction();
 			this.ibatis.insert("userRole.insertUserRole", userRoleBean);
 			this.ibatis.commitTransaction();
-		} finally{
+		} finally {
 			this.ibatis.endTransaction();
 		}
 	}
 
-	public UserRoleBean getUserRoleEdit(int roleId) throws SQLException{
-		UserRoleBean userRoleBean = (UserRoleBean) this.ibatis.queryForObject("userRole.getUserRoleEdit", roleId);
+	public UserRoleBean getUserRoleEdit(int roleId) throws SQLException {
+		UserRoleBean userRoleBean = (UserRoleBean) this.ibatis.queryForObject(
+				"userRole.getUserRoleEdit", roleId);
 		return userRoleBean;
 	}
 
-	public void editUserRole(UserRoleBean userRoleBean) throws SQLException{
+	public void editUserRole(UserRoleBean userRoleBean) throws SQLException {
 		try {
 			this.ibatis.startTransaction();
 			this.ibatis.update("userRole.editUserRole", userRoleBean);
 			this.ibatis.commitTransaction();
-		} finally{
+		} finally {
 			this.ibatis.endTransaction();
 		}
 	}
-	
-	public void deleteUserRole(Integer roleId) throws SQLException{
+
+	public void deleteUserRole(Integer roleId) throws SQLException {
 		try {
 			this.ibatis.startTransaction();
 			this.ibatis.delete("userRole.deleteUserRole", roleId);
 			this.ibatis.commitTransaction();
-		} finally{
+		} finally {
 			this.ibatis.endTransaction();
 		}
 	}
-	
-	public Integer getCountUserRole(String column, String value) throws SQLException, ClassNotFoundException {
+
+	public Integer getCountUserRole(String column, String value)
+			throws SQLException, ClassNotFoundException {
 		Map map = new HashMap();
-		map.put("col", column);
-		map.put("input", value);
-		Integer result = (Integer) this.ibatis.queryForObject("userRole.countUserRole", map);
+		map.put("searchField", column);
+		map.put("searchValue", value);
+		Integer result = (Integer) this.ibatis.queryForObject(
+				"userRole.countUserRole", map);
 		return result;
 	}
 }
