e24b56c5fa0c22cb27fbd6b2bb4a4779674db1ff
indra.as <indra.as@004ACE0315.nu-ace.ad-ins.com>
2015-06-12 09:34:29 +0700

    update rank +  user role

--- FinalProjectPromise/src/user/UserRoleBean.java
+++ FinalProjectPromise/src/user/UserRoleBean.java
@@ -5,44 +5,54 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UserRoleBean {
-	private Integer userRoleId;
-	private Integer updatedBy;
-	private Integer createdBy;
-	private String userRoleName;
-	private Date createDate;
-	private String createDateInString;
-	private Date updateDate;
-	private String updateDateInString;
-	
+	private Integer 	userRoleId;
+	private Integer 	updatedBy;
+	private Integer 	createdBy;
+	private String 		userRoleName;
+	private Date 		createDate;
+	private String 		createDateInString;
+	private Date 		updateDate;
+	private String 		updateDateInString;
+	private String 		userRoleCode;
+
 	SimpleDateFormat df = new SimpleDateFormat();
-	
+
 	public Integer getUserRoleId() {
 		return userRoleId;
 	}
+
 	public void setUserRoleId(Integer userRoleId) {
 		this.userRoleId = userRoleId;
 	}
+
 	public Integer getUpdatedBy() {
 		return updatedBy;
 	}
+
 	public void setUpdatedBy(Integer updatedBy) {
 		this.updatedBy = updatedBy;
 	}
+
 	public Integer getCreatedBy() {
 		return createdBy;
 	}
+
 	public void setCreatedBy(Integer createdBy) {
 		this.createdBy = createdBy;
 	}
+
 	public String getUserRoleName() {
 		return userRoleName;
 	}
+
 	public void setUserRoleName(String userRoleName) {
 		this.userRoleName = userRoleName;
 	}
+
 	public Date getCreateDate() {
 		return createDate;
 	}
+
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 
@@ -52,9 +62,11 @@ public class UserRoleBean {
 			this.createDateInString = "";
 		}
 	}
+
 	public String getCreateDateInString() {
 		return createDateInString;
 	}
+
 	public void setCreateDateInString(String createDateInString) {
 		this.createDateInString = createDateInString;
 
@@ -69,24 +81,28 @@ public class UserRoleBean {
 		}
 		this.createDate = date;
 	}
+
 	public Date getUpdateDate() {
 		return updateDate;
 	}
+
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
-		
+
 		if (updateDate != null) {
 			this.updateDateInString = df.format(updateDate.getTime());
 		} else {
 			this.updateDateInString = "";
 		}
 	}
+
 	public String getUpdateDateInString() {
 		return updateDateInString;
 	}
+
 	public void setUpdateDateInString(String updateDateInString) {
 		this.updateDateInString = updateDateInString;
-		
+
 		Date date = new Date();
 		try {
 			date = df.parse(updateDateInString);
@@ -97,5 +113,13 @@ public class UserRoleBean {
 			date = null;
 		}
 		this.updateDate = date;
+	}
+
+	public String getUserRoleCode() {
+		return userRoleCode;
+	}
+
+	public void setUserRoleCode(String userRoleCode) {
+		this.userRoleCode = userRoleCode;
 	}
 }
