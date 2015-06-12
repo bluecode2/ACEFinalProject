ac7ccb2f442b5a4cb2cc5a24edefdfc340635a47
juanrio <juanrio@012ACE0315.nu-ace.ad-ins.com>
2015-06-12 17:32:40 +0700

    general param

--- FinalProjectPromise/src/general/GeneralParamForm.java
+++ FinalProjectPromise/src/general/GeneralParamForm.java
@@ -9,6 +9,11 @@ public class GeneralParamForm extends ActionForm {
 	private ArrayList<GeneralParamBean> arrList = new ArrayList<GeneralParamBean>();
 	private String searchCategory;
 	private String searchItem;
+	private Integer page;
+	private Integer countPage;
+	private String task;
+	
+	private GeneralParamBean bean;
 
 	public ArrayList<GeneralParamBean> getArrList() {
 		return arrList;
@@ -33,4 +38,38 @@ public class GeneralParamForm extends ActionForm {
 	public void setSearchItem(String searchItem) {
 		this.searchItem = searchItem;
 	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public Integer getCountPage() {
+		return countPage;
+	}
+
+	public void setCountPage(Integer countPage) {
+		this.countPage = countPage;
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+
+	public GeneralParamBean getBean() {
+		return bean;
+	}
+
+	public void setBean(GeneralParamBean bean) {
+		this.bean = bean;
+	}
+	
+	
 }
