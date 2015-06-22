d85431e695bebf9372b769c49eaccf7ee6c54a74
cdoankz <cdoankz@gmail.com>
2015-06-22 18:40:34 +0700

    cemungud eaaaa

--- FinalProjectPromise/src/project_member/ProjectMemberBean.java
+++ FinalProjectPromise/src/project_member/ProjectMemberBean.java
@@ -5,7 +5,49 @@ public class ProjectMemberBean {
 	private Integer projectRoleId;
 	private Integer employeeId;
 	private Integer projectId;
+	private String projCode;
+	private String projName;
+	private String empName;
+	private String projRoleName;
+	private String projRoleCode;
+	private Integer isDeleted;
 	
+	public Integer getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(Integer isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public String getProjCode() {
+		return projCode;
+	}
+	public void setProjCode(String projCode) {
+		this.projCode = projCode;
+	}
+	public String getProjName() {
+		return projName;
+	}
+	public void setProjName(String projName) {
+		this.projName = projName;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getProjRoleName() {
+		return projRoleName;
+	}
+	public void setProjRoleName(String projRoleName) {
+		this.projRoleName = projRoleName;
+	}
+	public String getProjRoleCode() {
+		return projRoleCode;
+	}
+	public void setProjRoleCode(String projRoleCode) {
+		this.projRoleCode = projRoleCode;
+	}
 	public Integer getMemberId() {
 		return memberId;
 	}
