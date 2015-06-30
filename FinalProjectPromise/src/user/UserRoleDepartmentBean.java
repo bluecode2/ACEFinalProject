8b2f8371f93efb4401a4cb21ca1d82ea685dbd83
irene.di <irene.di@006ACE0315.nu-ace.ad-ins.com>
2015-06-30 18:40:14 +0700

    user role department

---
+++ FinalProjectPromise/src/user/UserRoleDepartmentBean.java
@@ -0,0 +1,22 @@
+package user;
+
+public class UserRoleDepartmentBean {
+	private Integer userRoleId;
+	private Integer deptId;
+	
+	
+	public Integer getUserRoleId() {
+		return userRoleId;
+	}
+	public void setUserRoleId(Integer userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+	public Integer getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+	
+	
+}
---
+++ FinalProjectPromise/src/user/UserRoleDepartmentManager.java
@@ -0,0 +1,84 @@
+package user;
+
+import ibatis.IbatisHelper;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import reports.ReportRoleBean;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+public class UserRoleDepartmentManager {
+private SqlMapClient ibatis;
+	
+	public UserRoleDepartmentManager() {
+		// TODO Auto-generated constructor stub
+		this.ibatis = IbatisHelper.getSqlMapInstance();
+	}
+	
+	public List<UserRoleDepartmentBean> getUserRoleDepartmentByUserRole(Integer roleId) {
+		
+		List<UserRoleDepartmentBean> arrList = new ArrayList<UserRoleDepartmentBean>();
+		
+		try {
+			arrList = this.ibatis.queryForList("userRoleDepartment.getUserRoleDepartmentByUserRole", roleId);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return arrList;
+	}
+	
+	public UserRoleDepartmentBean getUserRoleDepartment(Integer roleId, Integer deptId) {
+		UserRoleDepartmentBean rrBean = null;
+		
+		Map m = new HashMap();
+		m.put("roleId", roleId);
+		m.put("deptId", deptId);
+		
+		try {
+			rrBean = (UserRoleDepartmentBean) this.ibatis.queryForObject("userRoleDepartment.getUserRoleDepartment", m);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rrBean;
+	}
+	
+	public void insertUserRoleDepartment(UserRoleDepartmentBean rrBean){
+		try {
+			this.ibatis.startTransaction();
+			this.ibatis.insert("userRoleDepartment.insertUserRoleDepartment", rrBean);
+			this.ibatis.commitTransaction();
+		} catch(Exception e) {
+			e.printStackTrace();
+			try {
+				this.ibatis.endTransaction();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+	public void deleteUserRoleDepartment(UserRoleDepartmentBean rrBean) {
+		try {
+			this.ibatis.startTransaction();
+			this.ibatis.delete("userRoleDepartment.deleteUserRoleDepartment", rrBean);
+			this.ibatis.commitTransaction();
+		} catch(Exception e) {
+			e.printStackTrace();
+			try {
+				this.ibatis.endTransaction();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+}
