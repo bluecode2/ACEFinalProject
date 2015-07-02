9e13eb6f39118763b8610cbacde3663c41b3322b
bonaventura.aap <bonaventura.aap@003ACE0315.nu-ace.ad-ins.com>
2015-07-02 09:50:43 +0700

    constant

---
+++ PromiseHouseKeeping/src/common/Constant.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Constant {
+	public static class StringFormat{
+		public static final String dateFormat = "yyyy-MM-dd HH:mm:ss";
+	}
+}
--- PromiseHouseKeeping/src/project_log/ProjectLogBean.java
+++ PromiseHouseKeeping/src/project_log/ProjectLogBean.java
@@ -1,5 +1,90 @@
 package project_log;
 
-public class ProjectLogBean {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import common.Constant;
+
+public class ProjectLogBean {
+	private Integer projectLogId;
+	private Integer projectId;
+	private Integer updateBy;
+	private String projectField;
+	private String projectOldValue;
+	private String projectNewValue;
+	private Date updateDate;
+	private String remarks;
+	private String changeType;
+	
+	private String updateDateInString;
+	
+	SimpleDateFormat df = new SimpleDateFormat();
+	
+	public Integer getProjectLogId() {
+		return projectLogId;
+	}
+	public void setProjectLogId(Integer projectLogId) {
+		this.projectLogId = projectLogId;
+	}
+	public Integer getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+	public Integer getUpdateBy() {
+		return updateBy;
+	}
+	public void setUpdateBy(Integer updateBy) {
+		this.updateBy = updateBy;
+	}
+	public String getProjectField() {
+		return projectField;
+	}
+	public void setProjectField(String projectField) {
+		this.projectField = projectField;
+	}
+	public String getProjectOldValue() {
+		return projectOldValue;
+	}
+	public void setProjectOldValue(String projectOldValue) {
+		this.projectOldValue = projectOldValue;
+	}
+	public String getProjectNewValue() {
+		return projectNewValue;
+	}
+	public void setProjectNewValue(String projectNewValue) {
+		this.projectNewValue = projectNewValue;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+		if (updateDate != null){
+			this.updateDateInString = df.format(updateDate.getTime());
+		} else {
+			this.updateDateInString = "";
+		}
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	public String getChangeType() {
+		return changeType;
+	}
+	public void setChangeType(String changeType) {
+		this.changeType = changeType;
+	}
+	public String getUpdateDateInString() {
+		return updateDateInString;
+	}
+	public void setUpdateDateInString(String updateDateInString) {
+		this.updateDateInString = updateDateInString;
+	}
+	
+	
 }
