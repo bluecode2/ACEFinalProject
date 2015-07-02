c9a7846171f3710f1b78dfea7e4223e5f92e50ca
cdoankz <cdoankz@gmail.com>
2015-07-02 10:09:55 +0700

    audit trail

--- PromiseHouseKeeping/src/audit_trail/AuditTrailBean.java
+++ PromiseHouseKeeping/src/audit_trail/AuditTrailBean.java
@@ -1,5 +1,97 @@
 package audit_trail;
 
-public class AuditTrailBean {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import common.Constant;
+
+public class AuditTrailBean {
+	
+	private Integer auditTrailId;
+	private Integer updatedBy;
+	private String tableName;
+	private String fieldName;
+	private String oldValue;
+	private String newValue;
+	private String changeType;
+	private String dataId;
+	private Date updateDate;
+	private String updateDateInString;
+	
+	SimpleDateFormat sdf = new SimpleDateFormat(Constant.StringFormat.dateFormat);
+	
+	public Integer getAuditTrailId() {
+		return auditTrailId;
+	}
+	public void setAuditTrailId(Integer auditTrailId) {
+		this.auditTrailId = auditTrailId;
+	}
+	public Integer getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(Integer updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public String getOldValue() {
+		return oldValue;
+	}
+	public void setOldValue(String oldValue) {
+		this.oldValue = oldValue;
+	}
+	public String getNewValue() {
+		return newValue;
+	}
+	public void setNewValue(String newValue) {
+		this.newValue = newValue;
+	}
+	public String getChangeType() {
+		return changeType;
+	}
+	public void setChangeType(String changeType) {
+		this.changeType = changeType;
+	}
+	public String getDataId() {
+		return dataId;
+	}
+	public void setDataId(String dataId) {
+		this.dataId = dataId;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+		if (updateDate != null){
+			this.updateDateInString = sdf.format(updateDate.getTime());
+		} else {
+			this.updateDateInString = "";
+		}
+	}
+	public String getUpdateDateInString() {
+		return updateDateInString;
+	}
+	public void setUpdateDateInString(String updateDateInString) {
+		this.updateDateInString = updateDateInString;
+		Date date = new Date();
+		try {
+			date = sdf.parse(updateDateInString);
+		} catch (Exception pe){
+			
+			this.updateDateInString = "";
+			date = null;
+		}
+		this.updateDate = date;
+	}
 }
---
+++ PromiseHouseKeeping/src/audit_trail/AuditTrailManager.java
@@ -0,0 +1,29 @@
+package audit_trail;
+
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ibatis.IbatisHelper;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import common.Constant;
+
+public class AuditTrailManager {
+
+	private SqlMapClient ibatis;
+	SimpleDateFormat sdf = new SimpleDateFormat(Constant.StringFormat.dateFormat);
+	
+	public AuditTrailManager(){
+		this.ibatis = IbatisHelper.getSqlMapInstance();
+	}
+	
+	public List<AuditTrailBean> getAuditTrailByDate() throws SQLException{
+		List<AuditTrailBean> listAudit = new ArrayList<AuditTrailBean>();
+		listAudit = this.ibatis.queryForList("auditTrail.getAllAuditTrail", null);
+		
+		return listAudit;
+	}
+}
