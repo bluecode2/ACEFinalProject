a79967a5acbade09b81d85f240853acef0effb10
indra.as <indra.as@004ACE0315.nu-ace.ad-ins.com>
2015-07-07 15:30:19 +0700

    update

--- FinalProjectPromise/src/rank_employee/RankEmpManager.java
+++ FinalProjectPromise/src/rank_employee/RankEmpManager.java
@@ -18,21 +18,21 @@ public class RankEmpManager {
 		this.ibatis = IbatisHelper.getSqlMapInstance();
 	}
 
-	public List<RankEmpBean> getAllEmployeeRank(String col, String input,Integer pageNum, Integer pageSize) {
+	public List<RankEmpBean> getAllEmployeeRank(String col, String input,
+			Integer pageNum, Integer pageSize) {
 
-		
 		int begin = (pageNum - 1) * pageSize;
 		int end = pageNum * pageSize;
 
 		List<RankEmpBean> list = new ArrayList<RankEmpBean>();
-		
+
 		Map map = new HashMap();
 		map.put("searchField", col);
 		map.put("searchValue", input);
 		map.put("begin", begin);
 		map.put("end", end);
 
-		 try {
+		try {
 			list = this.ibatis.queryForList("rank.getAllEmployeeRank", map);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -41,8 +41,8 @@ public class RankEmpManager {
 		return list;
 	}
 
-	public Integer getCountRankEmp(String column, String value) throws SQLException
-			  {
+	public Integer getCountRankEmp(String column, String value)
+			throws SQLException {
 		Map map = new HashMap();
 		map.put("searchField", column);
 		map.put("searchValue", value);
@@ -50,8 +50,8 @@ public class RankEmpManager {
 				"rank.countEmployeeRank", map);
 		return count;
 	}
-	
-	public List<RankEmpBean> getListRankForSearch(String column, String value)   {
+
+	public List<RankEmpBean> getListRankForSearch(String column, String value) {
 		Map m = new HashMap();
 		m.put("searchField", column);
 		m.put("searchValue", value);
@@ -64,33 +64,36 @@ public class RankEmpManager {
 		}
 		return arrList;
 	}
-	
-	public RankEmpBean getRankByRankId(int tmpRankId)   {
-		RankEmpBean rankBean=null;
+
+	public RankEmpBean getRankByRankId(int tmpRankId) {
+		RankEmpBean rankBean = null;
 		try {
-			rankBean = (RankEmpBean) this.ibatis.queryForObject("rank.getRankByRankId", tmpRankId);
+			rankBean = (RankEmpBean) this.ibatis.queryForObject(
+					"rank.getRankByRankId", tmpRankId);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return rankBean;
 	}
-	
-	public int getNewRankId() throws SQLException   {
-		int NewRankId = (Integer) this.ibatis.queryForObject("rank.getNewRankId", null);
+
+	public int getNewRankId() throws SQLException {
+		int NewRankId = (Integer) this.ibatis.queryForObject(
+				"rank.getNewRankId", null);
 		return NewRankId;
 	}
-	
-	public void insertEmployeeRank(RankEmpBean eb)
-			  {
+
+	public boolean insertEmployeeRank(RankEmpBean eb) {
+		boolean flag = true;
 		try {
 			this.ibatis.startTransaction();
 			eb.setRankId(getNewRankId());
-			
+
 			this.ibatis.insert("rank.insertEmployeeRank", eb);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			flag = false;
 			try {
 				ibatis.endTransaction();
 			} catch (Exception e1) {
@@ -98,16 +101,18 @@ public class RankEmpManager {
 				e1.printStackTrace();
 			}
 		}
+		return flag;
 	}
 
-	public void updateEmployeeRank(RankEmpBean eb)
-			  {
+	public boolean updateEmployeeRank(RankEmpBean eb) {
+		boolean flag = true;
 		try {
 			this.ibatis.startTransaction();
 			this.ibatis.update("rank.updateEmployeeRank", eb);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			flag = false;
 			try {
 				ibatis.endTransaction();
 			} catch (Exception e1) {
@@ -115,16 +120,19 @@ public class RankEmpManager {
 				e1.printStackTrace();
 			}
 		}
+		return true;
 	}
 
-	public void deleteEmployeeRank(RankEmpBean eb)
-			  {
+	public boolean deleteEmployeeRank(RankEmpBean eb) {
+		boolean flag = true;
+		
 		try {
 			this.ibatis.startTransaction();
 			this.ibatis.update("rank.deleteEmployeeRank", eb);
 			this.ibatis.commitTransaction();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			flag = false;
 			try {
 				ibatis.endTransaction();
 			} catch (Exception e1) {
@@ -132,5 +140,6 @@ public class RankEmpManager {
 				e1.printStackTrace();
 			}
 		}
+		return flag;
 	}
 }
